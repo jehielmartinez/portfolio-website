4b2a134e8650c9ca108dbe36bf1d70b73234761c
Jehiel Martinez <jehielmartinez@gmail.com>
2019-08-13 09:43:36 -0600

    about component added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { Container, Col, Row } from 'react-bootstrap';
 
 import Profile from './components/Profile';
 import Skills from './components/Skills';
+import About from './components/About.js';
 
 
 function App() {
-  const {profile, skills} = resume
+  const {profile, skills, about} = resume
   return (
-    <Container style={{marginTop: 10}}>
+      <Container style={{marginTop: 10}}>
       <Row>
         <Col xs={12} md={4}>
           <Profile profile={profile}/>
         </Col>
         <Col xs={12} md={8}>
+          <About about={about}/>
           <Skills skills={skills}/>
         </Col>
       </Row>
