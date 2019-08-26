a361d904768b75c8cd30a8047d1fc0224b21781a
Jehiel Martinez <jehielmartinez@gmail.com>
2019-08-26 02:56:02 -0600

    placement of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import { Navbar } from 'react-bootstrap'
 
 export default function Footer(props) {
     return(
-        <Navbar className='justify-content-between' variant='dark' style={{backgroundColor: '#003d5b'}}>
-            <Navbar.Text> </Navbar.Text>
+        <Navbar variant='dark' className='justify-content-between' style={{backgroundColor: '#003d5b'}}>
             <Navbar.Text>Jehiel Martinez &copy; 2019</Navbar.Text>
             <Navbar.Text as='a' target='_blank' href='https://github.com/jehielmartinez/portfolio-website'>Source Code</Navbar.Text>
         </Navbar>
