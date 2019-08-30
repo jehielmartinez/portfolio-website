ec52f70fee9e9f9d40660cd26b9ba11d7e6f399a
Jehiel Martinez <jehielmartinez@gmail.com>
2019-08-30 12:34:09 -0600

    change interval from 2s to 4s

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardImg, CardDeck, Carousel, Row, Col } from 'react-bootstrap'
+import { Card, CardImg, Carousel, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrochip } from '@fortawesome/free-solid-svg-icons';
 
@@ -16,27 +16,27 @@ export default function Projects(props) {
                 <Card.Title><FontAwesomeIcon style={{marginRight: 10}} icon={faMicrochip}/>Projects</Card.Title>
             </Card.Header>
             <Card.Body>
-            <Carousel interval={2000}>
+            <Carousel interval={4000}>
                 
                     {projects.map((project, key) => (
                         <Carousel.Item>
-                        <Row>
-                            <Col xs={12} md={6}>
-                                <Card key={key} style={{margin: 5}}>
-                                    <CardImg src={`${path}/project-image/${project._id}`}/>
-                                    <Card.Header className='text-center'>
-                                        <Card.Title as='a' target='_blank' href={project.link} style={{color: 'black', fontWeight: 'bold', fontSize: 20}}>{project.name}</Card.Title>
-                                </Card.Header>
-                            </Card>
-                            </Col>
-                            <Col xs={12} md={6}>
-                            <Card key={key}>
-                                <Card.Body>
-                                    <Card.Text className='text-justify'>{project.description}</Card.Text>
-                                </Card.Body>
-                            </Card>
-                            </Col>
-                        </Row>
+                            <Row>
+                                <Col xs={12} md={6}>
+                                    <Card key={key} style={{margin: 5}}>
+                                        <CardImg src={`${path}/project-image/${project._id}`}/>
+                                        <Card.Header className='text-center'>
+                                            <Card.Title as='a' target='_blank' href={project.link} style={{color: 'black', fontWeight: 'bold', fontSize: 20}}>{project.name}</Card.Title>
+                                    </Card.Header>
+                                </Card>
+                                </Col>
+                                <Col xs={12} md={6}>
+                                <Card key={key}>
+                                    <Card.Body>
+                                        <Card.Text className='text-justify'>{project.description}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                                </Col>
+                            </Row>
                         </Carousel.Item>
                     ))}
                    
